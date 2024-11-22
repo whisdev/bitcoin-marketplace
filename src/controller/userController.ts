acb36479ae4e3bfe6abff61a37d551bdd8ec46d6
muffin819 <ch.engleongkai0310@gmail.com>
2024-11-21 21:37:26 -0800

    Get User Rune Information

--- src/controller/userController.ts
+++ src/controller/userController.ts
@@ -7,43 +7,15 @@ bitcoin.initEccLib(ecc);
 import { getBtcUtxoByAddress, getRuneBalanceListByAddress, getRuneUtxoByAddress } from "../service/service";
 import PoolInfoModal from "../model/PoolInfo";
 import { IRuneUtxo } from "../utils/type";
+import { OPENAPI_UNISAT_TOKEN } from "../config/config";
 
 dotenv.config();
 
 export const getUserRuneInfo = async (userAddress: string) => {
-    const poolInfoResult = await PoolInfoModal.find();
-
-    const poolRuneInfoSet = poolInfoResult.map(item => { return item.runeId });
-
-    console.log('poolRuneInfoSet :>> ', poolRuneInfoSet);
-    console.log('userAddress :>> ', userAddress);
-    const addressRuneBalance = await getRuneBalanceListByAddress(userAddress);
-    console.log('addressRuneBalance :>> ', addressRuneBalance);
-
-    const matchedRuneInfo = addressRuneBalance
-        ?.map(item => item.runeid)
-        .filter(runeId => poolRuneInfoSet.includes(runeId)) || [];
-
-    console.log('matchedRuneInfo :>> ', matchedRuneInfo);
-
-    const tempUserRuneInfo: any = await Promise.all(
-        matchedRuneInfo.map(async runeId => {
-            const { tokenSum } = await getRuneUtxoByAddress(userAddress, runeId);
-            return {
-                tokenType: "rune",
-                btcAmount: '',
-                ticker: '',
-                poolAddress: '',
-                runeAmont: tokenSum,
-                runeId: runeId,
-            };
-        })
-    );
-
-    const userBtcInfo = await getBtcUtxoByAddress(userAddress);
-
-    const userRuneInfo = tempUserRuneInfo;
-
+    const response = await getRuneBalanceListByAddress(userAddress);
+   
+    const userRuneInfo = response;
+    console.log('userRuneInfo :>> ', userRuneInfo);
     return {
         success: true,
         message: "get user rune info successfully",
